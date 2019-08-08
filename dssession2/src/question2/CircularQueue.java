b554c3c5a11e862ddb35197170b6587767bbe19d
Anmol Babbar <anmol.babbar@metacube.com>
2019-08-08 13:40:26 +0530

    question 2 corrected

--- dssession2/src/question2/CircularQueue.java
+++ dssession2/src/question2/CircularQueue.java
@@ -5,61 +5,98 @@ public class CircularQueue {
 	int[] circularQueue;
 	int start = -1, end = -1;
 
+	/**
+	 * Constructor of Circular queue for initialising the queue
+	 * 
+	 * @param length
+	 *            is the size of the queue
+	 */
 	public CircularQueue(int length) {
 		circularQueue = new int[length];
 
 	}
 
+	/**
+	 * addItem adds an item to the queue
+	 * 
+	 * @param value
+	 *            is the item to be added to queue
+	 * @return String of message
+	 */
 	public String addItem(int value) {
 		if (start == -1 && end == -1) {
 			circularQueue[0] = value;
 			start = end = 0;
-		} else if (start - end == 1) {
-			return "Queue Full";
-		} else if (end == circularQueue.length - 1 && start == 0) {
-			return "Queue Full";
-		}
-		else if (end == circularQueue.length - 1 && start != 0) {
-			end=0;
-			circularQueue[end]=value;
-		}else {
-			end++;
-			circularQueue[end]=value;
-		}
+		} else
+			try {
+				if (start - end == -1) {
+					throw new AssertionError("queue full");
+				} else if (end == circularQueue.length - 1 && start == 0) {
+					return "Queue Full";
+				} else if (end == circularQueue.length - 1 && start != 0) {
+					end = 0;
+					circularQueue[end] = value;
+				} else {
+					end++;
+					circularQueue[end] = value;
+				}
+			} catch (AssertionError e) {
+				return "Queue full";
+			}
 
-		return "added";
+		return "Added Element to Queue";
 
 	}
-	
-	public String deleteItem(){
-		if(start==-1)
-			return "empty";
-		else if(start==end){
-			circularQueue[start]=0;
-			start=-1;
-			end=-1;
+
+	/**
+	 * deleteItem delete the first element of the queue
+	 * 
+	 * @return message
+	 */
+	public String deleteItem() {
+		try {
+			if (start == -1) {
+				throw new AssertionError("Queue is Empty");
+				// return "Queue is Empty";
+			}
+		} catch (AssertionError e) {
+			return "Queue is empty";
 		}
-		else if(start==circularQueue.length-1){
-			start=0;
-			circularQueue[start]=0;
-		}
-		else {
-			circularQueue[start]=0;
+		if (start == end) {
+			circularQueue[start] = 0;
+			start = -1;
+			end = -1;
+		} else if (start == circularQueue.length - 1) {
+			start = 0;
+			circularQueue[start] = 0;
+		} else {
+			circularQueue[start] = 0;
 			start++;
 		}
-			
-			
-			return "deleted";
+
+		return "Deleted Element from Queue";
 	}
-	
-	public String checkEmptyOrFull(){
-		if ((start - end == 1) ||  (end == circularQueue.length - 1 && start == 0)) 
+
+	/**
+	 * checkEmptyorFull checks queue or space availability
+	 * 
+	 * @return message
+	 */
+	public String checkEmptyOrFull() {
+		try {
+			if ((start - end == 1)
+					|| (end == circularQueue.length - 1 && start == 0))
+				throw new AssertionError("Queue is full");
+		} catch (AssertionError e) {
 			return "Queue is Full";
-		return "Queue empty";
+		}
+		if (start == -1)
+			return "Queue is empty";
+		return "Queue has some Elements";
 	}
-	
-	public void print(){
-		for(int i=0;i<circularQueue.length;i++){
+
+	public void print() {
+		for (int i = 0; i < circularQueue.length; i++) {
 			System.out.println(circularQueue[i]);
 		}
 	}
--- dssession2/src/question2/Main.java
+++ dssession2/src/question2/Main.java
@@ -1,24 +1,51 @@
 package question2;
 
+import java.util.Scanner;
+
+
 public class Main {
 
-	public static void main(String[] args) {
-		CircularQueue c=new CircularQueue(5);
-		System.out.println(c.addItem(5));
-		System.out.println(c.addItem(1));
-		System.out.println(c.addItem(2));
-		System.out.println(c.addItem(3));
-		System.out.println(c.addItem(5));
-		//System.out.println(c.deleteItem());
-		//System.out.println(c.deleteItem());
-		//System.out.println(c.deleteItem());
-		//System.out.println(c.deleteItem());
-		//System.out.println(c.deleteItem());
-		//System.out.println(c.deleteItem());
-		System.out.println(c.addItem(11));
-		//System.out.println(c.deleteItem());
-		System.out.println(c.checkEmptyOrFull());
-		c.print();
+	private static Scanner sc;
+
+	public static void main(String[] args) throws Exception {
+
+		int choice, item;
+		sc = new Scanner(System.in);
+		CircularQueue queue = new CircularQueue(5);
+
+		do {
+			System.out.println("\n1.Insert an element");
+			System.out.println("2.Delete an element");
+			System.out.println("3.Display an element");
+			System.out.println("4.Check Queue is empty or Full or Has Elementss");
+			System.out.println("0.Quit");
+
+			System.out.print("Enter your choice : ");
+			choice = Integer.parseInt(sc.next());
+
+			switch (choice) {
+			case 1:
+				System.out.print("\nInput the element for insertion in queue : ");
+				item = Integer.parseInt(sc.next());
+				System.out.println(queue.addItem(item));
+				break;
+			case 2:
+				System.out.println(queue.deleteItem());
+				break;
+			case 3:
+				System.out.println("\nThe queue is : ");
+				queue.print();
+				break;
+			case 4:
+				System.out.println(queue.checkEmptyOrFull());
+				break;
+			default:
+				System.out.println("Wrong choice\n");
+			}
+		} while (choice != 0);
+
 	}
 
 }
+
+
