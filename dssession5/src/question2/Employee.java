43e54aa843e518ce5fb42a11c488f211d91491a1
Anmol Babbar <anmol.babbar@metacube.com>
2019-08-09 16:58:12 +0530

    ds session 5 assignment

---
+++ dssession5/src/question2/Employee.java
@@ -0,0 +1,38 @@
+package question2;
+
+public class Employee {
+
+	int employeeAge;
+	String employeeName;
+	double employeeSalary;
+	Employee next;
+
+	/**
+	 * Constructor to initialize the data members of the class
+	 * 
+	 * @param data
+	 */
+	public Employee(String employeeName, int employeeAge, double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+		this.employeeAge = employeeAge;
+		this.employeeName = employeeName;
+		this.next = null;
+	}
+
+	public int getEmployeeAge() {
+		return employeeAge;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public Employee getNext() {
+		return next;
+	}
+
+	public double getEmployeeSalaray() {
+		return employeeSalary;
+	}
+
+}
