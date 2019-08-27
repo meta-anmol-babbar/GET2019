d6764f81654ed321a38fba1d299887c806ffcbf2
Anmol Babbar <meta-anmol-babbar>
2019-08-28 01:01:07 +0530

    ead session 1 assignment

--- eadsession1/Showpricing.js
+++ eadsession1/Showpricing.js
@@ -1,14 +1,77 @@
-function showPricingCycle(){
-    var currencyList=document.getElementsByName("dropdown");
+function showPricing(){
     var dispLabel=document.getElementById("priceLabel");
     var pack;
+    var vehicleType=vehicleForm();
+    var daily,Monthly,Yearly;
     var type = [];
-    type  = document.getElementsByName("cyclepack");            
+    var e = document.getElementById("dropdown");
+    var currency = e.options[e.selectedIndex].value;
+    type  = document.getElementsByName("pack1");            
     for(i = 0; i < type.length; i++) { 
         if(type[i].checked) 
             pack = type[i].value; 
     }
-    if(pack="cycleDaily"){
-        dispLabel.innerHTML="5 INR";
+    if(currency=="INR"){
+     if(vehicleType=="cycle"){
+        daily=5;
+        Monthly=100;
+        Yearly=500;
     }
+    else if(vehicleType=="motorcycle"){
+        daily=10;
+        Monthly=200;
+        Yearly=1000;
+    }
+    else if(vehicleType=="4wheeler"){
+        daily=20;
+        Monthly=500;
+        Yearly=3500;
+    }
+}
+else if(currency=="YEN"){
+    if(vehicleType=="cycle"){
+       daily=7;
+       Monthly=147;
+       Yearly=740;
+   }
+   else if(vehicleType=="motorcycle"){
+       daily=15;
+       Monthly=295;
+       Yearly=1480;
+   }
+   else if(vehicleType=="4wheeler"){
+       daily=30;
+       Monthly=740;
+       Yearly=5175;
+   }
+}
+else if(currency=="Dollor"){
+    if(vehicleType=="cycle"){
+       daily=0.70;
+       Monthly=1.40;
+       Yearly=7;
+   }
+   else if(vehicleType=="motorcycle"){
+       daily=0.14;
+       Monthly=3;
+       Yearly=14;
+   }
+   else if(vehicleType=="4wheeler"){
+       daily=0.3;
+       Monthly=7;
+       Yearly=50;
+   }
+}
+        if(pack=="daily"){
+            dispLabel.innerHTML=daily+" "+currency;    
+        }
+        else if(pack=="monthly"){
+            dispLabel.innerHTML=Monthly+" "+currency;    
+        }
+        else if(pack=="yearly"){
+            dispLabel.innerHTML=Yearly+" "+currency;    
+        }
+    
+   
+    
 }
--- eadsession1/vehicleValidation.js
+++ eadsession1/vehicleValidation.js
@@ -1,10 +1,10 @@
 function vehicleForm(){
+    var vehicleType;
     var vehicleFromdiv = document.getElementById("vehicleForm");
     var togbutton=document.getElementById("submit");
     var namereg=/^[A-Za-z][A-Za-z]+/;
     var name = document.getElementById("vname").value;
     var vehicleNumber=document.getElementById("vnumber").value;
-    var vehicleType;
     var type = [];
     type  = document.getElementsByName("vtype");            
     for(i = 0; i < type.length; i++) { 
@@ -20,6 +20,6 @@ function vehicleForm(){
     else{
     vehicleFromdiv.style.display = "none";
      togbutton.innerHTML= "Show Element";
-     alert(" " + vehicleType);
+     return vehicleType;
     } 
 }
